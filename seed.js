34157d47952673b118922c21f99865e76b2d53d7
sanketpatel <patelsanketr98@gmail.com>
2024-01-06 19:54:59 -0500

    new API changed

--- seed.js
+++ seed.js
@@ -2,9 +2,10 @@ const axios = require("axios");
 const { addOrUpdateCharacter } = require("./dynamo");
 
 const seedData = async () => {
-  const url = "http://hp-api.herokuapp.com/api/characters";
+  const url = "https://hp-api.onrender.com/api/characters";
   try {
     const { data: characters } = await axios.get(url); // aliasing the output data(it comes from the specific api response api) as characters
+    console.log(characters);
     const characterPromises = characters.map((character, index) =>
       addOrUpdateCharacter({ ...character, id: index + '' })
     );
